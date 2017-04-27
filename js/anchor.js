f736ff9e3d22e82a9f78a45c29b2d5ba78db64c5
filippo04 <filippo0491@gmail.com>
2017-04-27 20:03:47 +0300

    portfolio

---
+++ js/anchor.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".dropdown-menu, #f, #cn").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
